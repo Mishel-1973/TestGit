88b0a62f5c1c403922375895a33f13fc2de12b73
maltez <nick.lototskiy@gmail.com>
2014-05-14 15:45:40 +0300

    Add functionality for mass transit transport

--- QueueFrameworksComparison/QFComparison/QFC.MassTransitTransport/MassTransitMessageReciever.cs
+++ QueueFrameworksComparison/QFComparison/QFC.MassTransitTransport/MassTransitMessageReciever.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using MassTransit;
+using QFC.Contracts.Configuration;
 using QFC.Contracts.Data;
 using QFC.Contracts.Interfaces;
 
 namespace QFC.MassTransitTransport
 {
-    public class MassTransitMessageReciever : IQueueReceiver<PocoClass>
+    public class MassTransitMessageReciever : IQueueReceiver<PocoClass>, IDisposable
     {
-        public void Subscribe()
+        private readonly IServiceBus _bus;
+        private static MassTransitMessageReciever _instance;
+        
+        private MassTransitMessageReciever(QueueConfig config)
         {
-            throw new NotImplementedException();
+            this._bus = ServiceBusFactory.New(cfg =>
+            {
+                cfg.UseRabbitMq();
+                cfg.ReceiveFrom(config.HostUrl);
+            });
+        }
+
+        public static MassTransitMessageReciever GetInstance(QueueConfig config)
+        {
+            return _instance ?? (_instance = new MassTransitMessageReciever(config));
         }
 
         public ConcurrentQueue<PocoClass> ReceivedData { get; private set; }
+
+        public void Subscribe()
+        {
+            this._bus.SubscribeHandler<PocoClass>(msg => ReceivedData.Enqueue(msg));
+        }
+
+        public void Dispose()
+        {
+            _bus.Dispose();
+        }
     }
 }
