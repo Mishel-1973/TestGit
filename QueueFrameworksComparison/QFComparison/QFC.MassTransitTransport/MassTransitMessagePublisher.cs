9ffded6e5346c4e97a1498257fd93f1010ca0ee9
maltez <nick.lototskiy@gmail.com>
2014-05-14 15:49:11 +0300

    Fix issues after the merge.

--- QueueFrameworksComparison/QFComparison/QFC.MassTransitTransport/MassTransitMessagePublisher.cs
+++ QueueFrameworksComparison/QFComparison/QFC.MassTransitTransport/MassTransitMessagePublisher.cs
@@ -1,31 +1,38 @@
-﻿using MassTransit;
+﻿using System;
+using MassTransit;
 using QFC.Contracts.Configuration;
 using QFC.Contracts.Data;
 using QFC.Contracts.Interfaces;
 
 namespace QFC.MassTransitTransport
 {
-    public class MassTransitMessagePublisher : IQueuePublisher<PocoClass>
+    public class MassTransitMessagePublisher : IQueuePublisher<PocoClass>, IDisposable
     {
-        private MassTransitMessagePublisher _instance;
+        private readonly IServiceBus _bus;
+        private static MassTransitMessagePublisher _instance;
 
         private MassTransitMessagePublisher(QueueConfig config)
         {
-            Bus.Initialize(cfg  =>
+            this._bus = ServiceBusFactory.New(cfg  =>
             {
                 cfg.UseRabbitMq();
                 cfg.ReceiveFrom(config.HostUrl);
             });
         }
 
-        public MassTransitMessagePublisher GetInstance(QueueConfig config)
+        public static MassTransitMessagePublisher GetInstance(QueueConfig config)
         {
             return _instance ?? (_instance = new MassTransitMessagePublisher(config));
         }
 
         public void Publish(PocoClass message)
         {
-                Bus.Instance.Publish(message);
+                this._bus.Publish(message);
+        }
+
+        public void Dispose()
+        {
+            _bus.Dispose();
         }
     }
 }
--- QueueFrameworksComparison/QFComparison/QFC_API/Data/PocoClass.cs
+++ QueueFrameworksComparison/QFComparison/QFC_API/Data/PocoClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MassTransit;
 
 namespace QFC.Contracts.Data
 {
