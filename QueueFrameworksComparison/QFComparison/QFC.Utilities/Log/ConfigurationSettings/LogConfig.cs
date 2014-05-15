92a64d6c3f062f20237aec410199259969a86799
maltez <nick.lototskiy@gmail.com>
2014-05-15 11:21:54 +0300

    Add loggin functionality

---
+++ QueueFrameworksComparison/QFComparison/QFC.Utilities/Log/ConfigurationSettings/LogConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace QFC.Utilities.Log.ConfigurationSettings
+{
+    public class LogConfig
+    {
+        public string SourceFilePath { get; set; }
+        public bool IsAppend { get; set; }
+    }
+}
