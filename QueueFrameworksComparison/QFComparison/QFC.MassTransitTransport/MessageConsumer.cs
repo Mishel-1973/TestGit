17e0e08bdb513d289ec5a349f9d7320093237697
maltez <nick.lototskiy@gmail.com>
2014-05-15 16:54:56 +0300

    Fix merge issues

---
+++ QueueFrameworksComparison/QFComparison/QFC.MassTransitTransport/MessageConsumer.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using MassTransit;
+using QFC.Contracts.Data;
+
+namespace QFC.MassTransitTransport
+{
+    public class MessageConsumer : Consumes<PocoClass>.All
+    {
+        public void Consume(PocoClass message)
+        {
+            MassTransitMessageReciever.MessageRecieved.Add(message);
+            Debug.WriteLine("Recieved message...");
+        }
+    }
+}
