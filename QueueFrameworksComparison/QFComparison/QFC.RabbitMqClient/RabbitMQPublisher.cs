5a0a0c8dae9d28be08678f130859929d93b0c769
Mishel-1973 <mishel.gurkin@gmail.com>
2014-05-16 12:49:58 +0300

    RabbitMQ using project

---
+++ QueueFrameworksComparison/QFComparison/QFC.RabbitMqClient/RabbitMQPublisher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using QFC.Contracts.Configuration;
+using QFC.Contracts.Data;
+using QFC.Contracts.Interfaces;
+using QFC.Utilities.Serialization.Serializators;
+using RabbitMQ.Client;
+
+namespace QFC.RabbitMqClient
+{
+	public class RabbitMqPublisher : IQueuePublisher<PocoClass>, IDisposable 
+	{
+        private static RabbitMqPublisher _instance;
+		private readonly QueueConfig _config;
+		private readonly IConnection _connection;
+		private readonly IModel _channel;
+		private readonly JSonSerializer<PocoClass> _serializer;
+
+        private RabbitMqPublisher(QueueConfig config)
+        {
+			_serializer = new JSonSerializer<PocoClass>();
+			_config = config;
+			var factory = new ConnectionFactory { HostName = _config.HostUrl };
+			_connection = factory.CreateConnection();
+			_channel = _connection.CreateModel();
+			_channel.QueueDeclare(_config.SubscriberId, false, false, false, null);
+        }
+
+        public static RabbitMqPublisher GetInstance(QueueConfig config)
+        {
+            return _instance ?? (_instance = new RabbitMqPublisher(config));
+        }
+
+		public void Publish(PocoClass message)
+		{
+			_channel.BasicPublish(string.Empty, _config.SubscriberId, null, Encoding.UTF8.GetBytes(_serializer.Serialize(message)));
+		}
+
+		public void Dispose()
+		{
+			_channel.Dispose();
+			_connection.Dispose();
+		}
+	}
+}
---
+++ QueueFrameworksComparison/QFComparison/QFC.RabbitMqClient/RabbitMQReceiver.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using QFC.Contracts.Configuration;
+using QFC.Contracts.Data;
+using QFC.Contracts.Interfaces;
+using QFC.Utilities.Log.ConfigurationSettings;
+using QFC.Utilities.Log.Contracts;
+using QFC.Utilities.Log.Logers;
+using QFC.Utilities.Serialization.Serializators;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace QFC.RabbitMqClient
+{
+	public class RabbitMQReceiver : IQueueReceiver<PocoClass>, IDisposable
+	{
+		private const int DataReadyTimeout = 10;
+		private readonly ILoger<PocoClass> _loger;
+		private readonly ConcurrentQueue<PocoClass> _data;
+		private readonly IConnection _connection;
+		private readonly IModel _channel;
+		private QueueingBasicConsumer _consumer;
+		private readonly Thread _listenerThread;
+		private static RabbitMQReceiver _instance;
+		private bool _disconnect;
+		private readonly QueueConfig _config;
+
+		private RabbitMQReceiver(QueueConfig config)
+		{
+			_config = config;
+            var logConfig = new LogConfig
+            {
+                IsAppend = true,
+                SourceFilePath = config.LogFilePath
+            };
+
+            _loger = new JsonLoger<PocoClass>(logConfig);
+            _data = new ConcurrentQueue<PocoClass>();
+
+			var factory = new ConnectionFactory { HostName = config.HostUrl };
+			_connection = factory.CreateConnection();
+			_channel = _connection.CreateModel();
+			this._listenerThread = new Thread(this.ListenerThreadFunc)
+			{
+				Priority = ThreadPriority.Normal,
+				Name = "RabbitMQ listener thread"
+			};
+			_disconnect = false;
+        }
+
+		public static RabbitMQReceiver GetInstance(QueueConfig config)
+        {
+			return _instance ?? (_instance = new RabbitMQReceiver(config));
+        }
+
+		public void Subscribe()
+		{
+			_channel.QueueDeclare(_config.SubscriberId, false, false, false, null);
+			_consumer = new QueueingBasicConsumer(_channel);
+			_channel.BasicConsume(_config.SubscriberId, true, _consumer);
+			this._listenerThread.Start();
+		}
+
+		public ConcurrentQueue<PocoClass> ReceivedData {
+			get { return _data; }
+		}
+
+		public void Dispose()
+		{
+			_disconnect = true;
+			this._listenerThread.Join(1000);
+			_channel.Dispose();
+			_connection.Dispose();
+		}
+
+		private void ListenerThreadFunc()
+		{
+			var  serializer = new JSonSerializer<PocoClass>();
+
+			while (!this._disconnect)
+			{
+				BasicDeliverEventArgs ea;
+				if (_consumer.Queue.Dequeue(DataReadyTimeout, out ea))
+				{
+					var body = ea.Body;
+					var message = Encoding.UTF8.GetString(body);
+					PocoClass temporaryObject = serializer.Deserialize(message);
+					this._loger.LogData(temporaryObject);
+					this.ReceivedData.Enqueue(temporaryObject);
+				}
+			}
+		}
+
+	}
+}
